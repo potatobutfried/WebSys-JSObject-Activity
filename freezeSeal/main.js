eb4d2a5344b4a5b78a22c2eae34dff0d8296c2ad
potatobutfried <136866351+potatobutfried@users.noreply.github.com>
2025-02-17 10:32:16 +0800

    ObjectFreezeAndObjectseal

---
+++ freezeSeal/main.js
@@ -0,0 +1,54 @@
+const user = {
+    userid: 'user123',
+    personalInfo: {
+        firstName:'John',
+        lastname:'Doe',
+        email:'john.doe@example.com'
+    },
+    preferences:{
+        theme: 'light',
+        notificationEnabled: true
+    },
+    securitySettings:{
+        lastLogin:'2024-07-04',
+        isAccountActive: true
+    }
+    
+};
+//Object freeze user id
+Object.freeze(user);
+user.userid = '001';
+console.log(user.userid);
+
+//Object Seal email
+Object.seal(user.personalInfo);
+user.personalInfo.email = 'jeremysabino75@gmail.com';
+console.log(user.personalInfo.email);
+Object.seal(user.personalInfo);
+user.personalInfo.firstName = 'jeremy';
+console.log(user.personalInfo.firstName);
+Object.seal(user.personalInfo);
+user.personalInfo.lastname = 'sabino';
+console.log(user.personalInfo.lastname);
+delete user.personalInfo.email;
+
+
+//object seal preferences
+Object.seal(user.preferences);
+user.preferences.theme = 'dark',
+console.log(user.preferences.theme);
+
+//object seal preferences
+Object.seal(user.securitySettings);
+user.preferences.notificationEnabled = false
+console.log(user.preferences.notificationEnabled);
+
+//date
+Object.seal(user.securitySettings);
+user.securitySettings.lastLogin = "2024-09-23"
+console.log(user.securitySettings.lastLogin);
+
+//security setting is account active
+Object.seal(user.securitySettings);
+user.securitySettings.isAccountActive = false
+console.log(user.securitySettings.isAccountActive);
